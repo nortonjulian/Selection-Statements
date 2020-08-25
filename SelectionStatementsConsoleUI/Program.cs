29a046b0ac424ba182d25ecf7ee7bf53a48f1fa1
Julian Norton <juliannorton@MacBook-Pro-3.lan>
2020-08-25 13:00:32 -0400

    If statements

---
+++ SelectionStatementsConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SelectionStatementsConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine($"Too High!");
+            }
+            else if(userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low!");
+            }
+            else
+            {
+                Console.WriteLine($"Nevermind");   
+            }
+        }
+    }
+}
